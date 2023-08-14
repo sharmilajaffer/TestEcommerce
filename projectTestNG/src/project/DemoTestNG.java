767bc4556803ca54a0270014a05f12acf8c9aee7
sharmeejaffer <sharmila.jafferalis@gmail.com>
2023-08-14 12:53:50 -0400

    Removed all commented code

--- projectTestNG/src/project/DemoTestNG.java
+++ projectTestNG/src/project/DemoTestNG.java
@@ -1,18 +1,9 @@
 package project;
 
-import org.testng.annotations.Test;
-
-
-
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.support.ui.Select;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebElement;
@@ -36,58 +27,51 @@ public class DemoTestNG {
 		  	  driver.get(url);
 		  	  driver.navigate().to(url);  
 		  	driver.manage().window().maximize();
-		  	//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
   	}
   	
 	@Test
   	public void test() throws InterruptedException {
              driver.findElement(By.xpath("//button[@type='submit']")).click();
-             //WebDriverWait wait = new WebDriverWait(driver,30);
-             //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/a[@class='nav-link active']//*[contains(text(),'Catalog')][1]")));
-		  	// Navigate to Categories
-		        driver.findElement(By.linkText("Catalog")).click();
+            
+		  	// Navigate to 'Catalog' Link on the left tree view 
+		      driver.findElement(By.linkText("Catalog")).click();
+		      
+		    // Navigate to 'Products' link in the left tree view  
 		      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));  
 		  	  driver.findElement(By.linkText("Products")).click();
-		  	  //WebElement m = driver.findElement(By.xpath("//a[@class='nav-link active']//*[contains(text(),'Catalog')][1]"));
-		  	  //m.click();
-		  	  //System.out.println("Test successfull");
-		        //driver.findElement(By.cssSelector("p").linkText(" Products"));
-		        //driver.findElement(By.cssSelector("a")).findElement(By.linkText(" Catalog")).click();
-		        //driver.findElement(By.linkText("Categories")).click();
-		        
-		        // Click on "Add New"
-		  	    driver.findElement(By.linkText("Add new")).click();
-		  	    driver.findElement(By.xpath("//input[@id='Name']")).sendKeys("HP-Laptop2");
-		  	    JavascriptExecutor js = (JavascriptExecutor)driver;
-		  	    js.executeScript("scroll(0, 500);");
+		  	  
+		    // Click on "Add New" button in Products screen 
+		  	  driver.findElement(By.linkText("Add new")).click();
+		  	  
+		  	// Add Product name  
+		  	  driver.findElement(By.xpath("//input[@id='Name']")).sendKeys("HP-Laptop2");
+		  	  JavascriptExecutor js = (JavascriptExecutor)driver;
+		  	
+		  	  // Scroll to locate Category select box
+		  	  js.executeScript("scroll(0, 500);");
 		  	    
-			 driver.findElement(By.xpath("//div[@class='form-group row']//div[@class='k-widget k-multiselect k-multiselect-clearable']")).click();
-		  	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-			 driver.findElement(By.xpath("//div[@id='SelectedCategoryIds-list']//ul/li[1]")).click();
-		  	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(55));
+			// Select 'Computers' Category  
+		  	  driver.findElement(By.xpath("//div[@class='form-group row']//div[@class='k-widget k-multiselect k-multiselect-clearable']")).click();
+		  	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
+			  driver.findElement(By.xpath("//div[@id='SelectedCategoryIds-list']//ul/li[1]")).click();
+		  	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
 
-
-		  	 // driver.findElement(By.xpath("//body/div[3]/div[1]/form[1]/section[1]/div[1]/div[1]/nop-cards[1]/nop-card[1]/div[1]/div[2]/div[2]/div[2]/div[1]/div[1]")).sendKeys("Computers");
-		  	js.executeScript("scroll(0, 1000);");  
-		  	  //driver.findElement(By.xpath("//div[@class='k-multiselect-wrap k-floatwrap']")).sendKeys("Computers");
-		  	    //driver.findElement(By.xpath("//*[@id='SelectedCategoryIds']")).sendKeys("Computers");
-		  	    //Thread.sleep(60);
-		  	    //driver.findElement(By.xpath("//*[@id='SelectedCategoryIds']")).findElement(By.xpath("//li[contains(text(),'Computers')]")).click();
-		  	  //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
-		  	    //driver.findElement(By.xpath())
-
-		  	driver.findElement(By.xpath("//body/div[3]/div[1]/form[1]/section[1]/div[1]/div[1]/nop-cards[1]/nop-card[2]/div[1]/div[2]/div[1]/div[2]/span[1]/span[1]/input[1]")).sendKeys("5000");
-		  	    //driver.findElement(By.xpath("//input[@id='Price']")).sendKeys("5000");
-		  	Thread.sleep(60);
-		  	    driver.findElement(By.xpath("//button[@name='save']")).click();
-		  	    driver.findElement(By.linkText("Categories")).click();
-		  	    driver.findElement(By.xpath("//input[@id='SearchCategoryName']")).sendKeys("Computers");
-		  	    Thread.sleep(60);
-		  	    driver.findElement(By.xpath("//button[@id='search-categories']")).click();
-		  	    
-		        //driver.findElement(By.cssSelector("button[onclick='AddCategory();']")).click();
-		  	  System.out.println("Test successfull");
-		  	 
+            // Scroll to locate Price input field		  	
+		  	  js.executeScript("scroll(0, 1000);");
+		  	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
+		      driver.findElement(By.xpath("//body/div[3]/div[1]/form[1]/section[1]/div[1]/div[1]/nop-cards[1]/nop-card[2]/div[1]/div[2]/div[1]/div[2]/span[1]/span[1]/input[1]")).sendKeys("5000");
+		  	  
+	        // Scroll up and Save the product 
+		      js.executeScript("scroll(0, 0);");
+		      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
+		      driver.findElement(By.xpath("//button[@name='save']")).click();
+		  	  
+	        // Search the Product in Category screen  
+	          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));  
+	          driver.findElement(By.linkText("Categories")).click();
+		  	  driver.findElement(By.xpath("//input[@id='SearchCategoryName']")).sendKeys("Computers");
+		      driver.findElement(By.xpath("//button[@id='search-categories']")).click();
+		  	  
   }
   
   @BeforeMethod
@@ -97,6 +81,7 @@ public class DemoTestNG {
  
   @AfterMethod
   public void afterMethod() {
+      System.out.println("Test successfull");
   	  System.out.println("Closing the browser session");
   	  driver.quit();
   }
